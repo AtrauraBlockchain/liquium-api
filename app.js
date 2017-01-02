9141269dc6be4fd88f0d4dd083682d4cd0c7c7d8
Jordi Baylina <jordi@baylina.cat>
2017-01-02 12:55:26 +0100

    First operative version

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+
 var app = express();
 var port = 8080;
+
+
+app.use(bodyParser.json());
+
 
 var liquiumRT = require('liquium-contracts');
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-app.all('*', function(req, res, next) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.get('/api/organization/:idOrganization/polls', function (req, res, next) {
@@ -20,6 +28,13 @@ app.get('/api/organization/:idOrganization/polls', function (req, res, next) {
 
 app.get('*', function(req, res) {
     res.end('404');
+});
+
+app.use(function (err, req, res, next) {
+    res.json({
+        result: "ERROR",
+        errorStr: err.toString()
+    });
 });
 
 server = app.listen(port, function() {
