cff2dc17cc29d5b0f71f1b3c156751cbd1a8b032
Jordi Baylina <jordi@baylina.cat>
2017-01-03 17:04:30 +0100

    extended api version

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -11,19 +13,27 @@ app.use(bodyParser.json());
 
 var liquiumRT = require('liquium-contracts');
 var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
-app.get('/api/organization/:idOrganization/polls', function (req, res, next) {
-    liquiumRT.getPolls(web3, req.params.idOrganization, function(err, polls) {
+app.get('/api/organization/:idOrganization', function (req, res, next) {
+  if (req.query.voter) {
+    if (!web3.isAddress(req.query.voter)) return next(new Error("Invalid voter address"));
+    liquiumRT.getAllInfo(web3, req.params.idOrganization, req.query.voter,function(err, st) {
         if (err) return next(err);
-        res.json(polls);
+        res.json(st);
     });
+  } else {
+    liquiumRT.getOrganizationInfo(web3, req.params.idOrganization, function(err, st) {
+        if (err) return next(err);
+        res.json(st);
+    });
+  }
 });
 
 app.get('*', function(req, res) {
@@ -37,7 +47,7 @@ app.use(function (err, req, res, next) {
     });
 });
 
-server = app.listen(port, function() {
+var server = app.listen(port, function() {
     var port = server.address().port;
     console.log('server listening at http://localhost:' + port.toString());
 });
